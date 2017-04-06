37b42681e64ff2a76108e104adbb370a4c9e0a96
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2017-04-07 02:18:24 +0800

    update with lalith code

--- demo/Debug/src/main.d
+++ demo/Debug/src/main.d
@@ -4,19 +4,18 @@ src/main.d: ../src/main.c \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc_types.h \
- C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_gpio.h \
- C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_i2c.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_ssp.h \
+ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_gpio.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_timer.h \
- C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/joystick.h \
+ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_i2c.h \
+ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_uart.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/pca9532.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/acc.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/oled.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/rgb.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/light.h \
  C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/temp.h \
- C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/led7seg.h \
- C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_uart.h
+ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/led7seg.h
 
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_pinsel.h:
 
@@ -28,15 +27,15 @@ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_CMSISv1p30_LPC17xx\inc/sys
 
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc_types.h:
 
-C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_gpio.h:
-
-C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_i2c.h:
-
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_ssp.h:
+
+C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_gpio.h:
 
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_timer.h:
 
-C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/joystick.h:
+C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_i2c.h:
+
+C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_uart.h:
 
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/pca9532.h:
 
@@ -51,5 +50,3 @@ C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/light.h:
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/temp.h:
 
 C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_EaBaseBoard\inc/led7seg.h:
-
-C:\Users\Sherry\ Wong\Desktop\EE2024\assignment_2\Lib_MCU\inc/lpc17xx_uart.h:
